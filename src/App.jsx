e8f4a69f0188e5fda59329f786fc2b6180073e08
ArturRibeiro01 <profissional.arturrc@gmail.com>
2022-04-11 14:50:28 -0300

    incialização do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">Gerador de Senhas</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
